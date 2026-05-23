469e8204a2cc8bb5e18c385ddbdcd7ba7dc35a93
khurrotulaeni <qurrotulaeni281@gmail.com>
2026-05-23 12:47:42 +0700

    fix bngt

--- src/App.tsx
+++ src/App.tsx
@@ -15,24 +15,28 @@ import DashboardIndex from "./pages/dashboard/DashboardIndex";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import DashboardLayout from "./layouts/DashboardLayout";
 
+import CategoryIndex from "./pages/dashboard/category/CategoryIndex";
 import CategoryCreate from "./pages/dashboard/category/CategoryCreate";
+import CategoryEdit from "./pages/dashboard/category/CategoryEdit";
+
 import FilmIndex from "./pages/dashboard/filmnetflix/FilmIndex";
 import FilmCreate from "./pages/dashboard/filmnetflix/FilmCreate";
 import FilmEdit from "./pages/dashboard/filmnetflix/FillmEdit";
-import CategoryIndex from "./pages/dashboard/category/CategoryIndex";
-import CategoryEdit from "./pages/dashboard/category/CategoryEdit";
+
 import EventIndex from "./pages/dashboard/event/EventIndex";
 import EventCreate from "./pages/dashboard/event/EventCreate";
 import EventEdit from "./pages/dashboard/event/EventEdit";
+
 import PembicaraIndex from "./pages/dashboard/pembicara/PembicaraIndex";
 import PembicaraCreate from "./pages/dashboard/pembicara/pembicaraCreate";
 import PembicaraEdit from "./pages/dashboard/pembicara/PembicaraEdit";
+
 import Biodata from "./pages/dashboard/biodata/Biodata";
 
 function App() {
   return (
     <Routes>
-
+      {/* Public Routes */}
       <Route element={<MainLayout />}>
         <Route path="/" element={<Homepage />} />
         <Route path="/arena" element={<Arena />} />
@@ -41,87 +45,41 @@ function App() {
         <Route path="/studio" element={<Studio />} />
       </Route>
 
+      {/* Authentication Routes */}
       <Route element={<AuthLayout />}>
-        <Route
-          path="/login"
-          element={<LoginForm />}
-        />
-
-        <Route
-          path="/register"
-          element={<RegisterForm />}
-        />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
       </Route>
 
+      {/* Protected Dashboard Routes */}
       <Route element={<ProtectedRoute />}>
         <Route element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<DashboardIndex />} />
 
-          <Route
-            path="/dashboard"
-            element={<DashboardIndex />}
-          />
-
-          <Route
-            path="/dashboard/category"
-            element={<CategoryIndex />}
-          />
-
-          <Route
-            path="/dashboard/category/create"
-            element={<CategoryCreate />}
-          />
-
-          <Route
-            path="/dashboard/category/edit/:id"
-            element={<CategoryEdit />}
-          />
+          {/* Category Management */}
+          <Route path="/dashboard/category" element={<CategoryIndex />} />
+          <Route path="/dashboard/category/create" element={<CategoryCreate />} />
+          <Route path="/dashboard/category/edit/:id" element={<CategoryEdit />} />
           
-          <Route
-            path="/dashboard/filmnetflix"
-            element={<FilmIndex />}
-          />
+          {/* Film Netflix Management */}
+          <Route path="/dashboard/filmnetflix" element={<FilmIndex />} />
+          <Route path="/dashboard/filmnetflix/create" element={<FilmCreate />} />
+          <Route path="/dashboard/filmnetflix/edit/:id" element={<FilmEdit />} />
 
-           <Route
-            path="/dashboard/filmnetflix/create"
-            element={<FilmCreate />}
-          />
+          {/* Event Management */}
+          <Route path="/dashboard/event" element={<EventIndex />} />
+          <Route path="/dashboard/event/create" element={<EventCreate />} />
+          <Route path="/dashboard/event/edit/:id" element={<EventEdit />} />
 
-          <Route
-          path="/dashboard/filmnetflix/edit/:id"
-          element={<FilmEdit />}
-        />
-          <Route
-            path="/dashboard/event"
-            element={<EventIndex />}
-          />
-          <Route
-            path="/dashboard/event/edit/:id"
-            element={<EventEdit />}
-          />
-          <Route
-            path="/dashboard/event/create"
-            element={<EventCreate />}
-          />
+          {/* Pembicara Management */}
+          <Route path="/dashboard/pembicara" element={<PembicaraIndex />} />
+          <Route path="/dashboard/pembicara/create" element={<PembicaraCreate />} />
+          <Route path="/dashboard/pembicara/edit/:id" element={<PembicaraEdit />} />
 
-          <Route
-            path="/dashboard/pembicara"
-            element={<PembicaraIndex />}
-          />
-          <Route
-            path="/dashboard/pembicara/create"
-            element={<PembicaraCreate />}
-          />
-          <Route
-            path="/dashboard/pembicara/edit/:id"
-            element={<PembicaraEdit />}
-          />
-          <Route
-            path="/dashboard/biodata"
-            element={<Biodata />}
-          />
+          {/* Biodata */}
+          <Route path="/dashboard/biodata" element={<Biodata />} />
         </Route>
       </Route>
-
     </Routes>
   );
 }
